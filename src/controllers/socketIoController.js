a60103aff528ce25f2e3c2e46ccff48e9573a35b
Gabriel Reis <gabsrs@gitbasic.com>
2024-07-19 11:38:10 -0300

    feat: Socket IO - Initial Project #1 - SocketIo events from search page
    - Handling events data in page

--- src/controllers/socketIoController.js
+++ src/controllers/socketIoController.js
@@ -1,10 +1,31 @@
+import { SearchArtistService } from "../services/spotifyApi/searchArtistService.js"
+import { TopTrackService } from "../services/spotifyApi/topTrackService.js"
+import { AlbumService } from "../services/spotifyApi/albumService.js"
+
 class SocketIoController {
     connection (socket) {
         console.log('a user connected');
+        const searchArtistService = new SearchArtistService()
+        const topTrackService = new TopTrackService()
+        const albumService = new AlbumService()
 
         socket.on('disconnect', () => {
             console.log('user disconnected');
         });
+
+        socket.on("searchingArtists", async (artistName) => {
+            const response = await searchArtistService.handle(artistName)
+            
+            socket.emit("searchResponse", response)
+        })
+
+        socket.on("getArtist", async (artistName) => {
+            const artistInfos = await searchArtistService.handle(artistName)
+            const topTracks = await topTrackService.handle(artistInfos[0].id)
+            const albumImg = await albumService.handle(artistInfos[0].id)
+
+            socket.emit("topTracks", artistInfos, topTracks, albumImg)
+        })
     }
 }
 
